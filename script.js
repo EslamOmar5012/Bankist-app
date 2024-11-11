b27d064edf517e593bbdee92686da84a175004c3
Eslam Omar <eslam.O.elkhabery@gmail.com>
2024-11-11 04:45:57 +0200

    update

--- script.js
+++ script.js
@@ -370,3 +370,10 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 // calcAverageHumanAge(dogs_data1);
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// //example on find() method
+// //The find() method of Array instances returns the first element in the provided array that satisfies the provided testing function. If no values satisfy the testing function, undefined is returned.
+
+// const firstWithdraw = movements.find(mov => mov < 0);
+// console.log(firstWithdraw);
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
